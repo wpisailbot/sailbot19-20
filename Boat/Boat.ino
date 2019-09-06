b043bb18d85bd004f7f315aeb83fd30d9f994dd9
Irina Lavryonova <lavryonovairina@gmail.com>
2019-09-05 22:44:19 -0400

    Establish rudimentary communication between the boat and the rigid sail.

--- Boat/Boat.ino
+++ Boat/Boat.ino
@@ -1,6 +1,111 @@
-void setup() {
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Servo.h>
+#include <EthernetUdp.h>
 
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+Servo servo;
+
+const int servo_pin = 9;
+
+IPAddress ip(192, 168, 0, 21);
+
+EthernetServer server(8888);
+
+EthernetUDP Udp;
+
+unsigned int localPort = 8888;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  // Set up the server on the arduino
+  Ethernet.begin(mac, ip);
+  
+   // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+  
+  server.begin();
+
+  // set up the udp connection to send messages to the teensy
+  Udp.begin(localPort);
+  
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+  Serial.print("UDP communication is located at port ");
+  Serial.println(localPort);
 }
 
 void loop() {
+  // listen for incoming clients
+  int udpStatus = Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  if(udpStatus){
+    //Serial.println("there was a problem resolving the hostname or port");
+    Serial.print("RemoteIP: ");
+    Serial.print(Udp.remoteIP());
+    Serial.print(" RemotePort: ");
+    Serial.println(Udp.remotePort());
+  }
+ 
+  EthernetClient client = server.available();
+  
+  if (client) {
+    // send a UDP message to the teensy
+    Udp.write("hello");
+  
+    // create the http page  
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println("Refresh: 5");
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.print("<h1>Output Values</h1>");
+          for (int digitalChannel = 2 ; digitalChannel < 10; digitalChannel++) {
+            int sensorReading = digitalRead(digitalChannel);
+            client.print("Output value ");
+            client.print(digitalChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(1);
+    Udp.endPacket();
+    Serial.println("sent same UDP packet");
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
--- RigidSail/RigidSail.ino
+++ RigidSail/RigidSail.ino
@@ -1,6 +1,148 @@
-void setup() {
+/*
+ WiFiEsp example: WebClient
+ This sketch connects to google website using an ESP8266 module to
+ perform a simple web search.
+ For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
+*/
 
+#include "WiFiEsp.h"
+
+// Emulate ESPSerial on pins 6/7 if not present
+//#ifndef HAVE_HWESPSerial
+#include "SoftwareSerial.h"
+SoftwareSerial ESPSerial(9, 10); // RX, TX
+//#endif
+
+char ssid[] = "sailbot";            // your network SSID (name)
+char pass[] = "Passphrase123";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+//char server[] = "192.168.0.25";
+IPAddress server(192, 168, 0, 21);
+int serverPort = 8888;
+
+unsigned long lastConnectionTime = 0;         // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10000L; // delay between updates, in milliseconds
+
+// Initialize the Ethernet client object
+WiFiEspClient client;
+
+void setup()
+{
+  // initialize serial for debugging
+  Serial.begin(115200);
+  // initialize serial for ESP module
+  ESPSerial.begin(115200);
+  setBaud("115200");
+  // initialize ESP module
+  WiFi.init(&ESPSerial);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+  
+  printWifiStatus();
+
+  Serial.println();
+  Serial.println("Starting connection to server...");
+  // if you get a connection, report back via serial
+  if (client.connect(server, serverPort)) {
+    delay(500);
+    Serial.println("Connected to server");
+    // Make a HTTP request
+    client.println("GET /values HTTP/1.1");
+    client.println("Host: 192.168.0.25:8888");
+    client.println("Connection: keep-alive");
+    client.println();
+  }
 }
 
-void loop() {
+void loop()
+{
+  // if there are incoming bytes available
+  // from the server, read them and print them
+  while (client.available()) {
+    Serial.println("Client available");
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if 10 seconds have passed since your last connection,
+  // then connect again and send data
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
+}
+
+// this method makes a HTTP connection to the server
+void httpRequest()
+{
+  Serial.println();
+    
+  // close any connection before send a new request
+  // this will free the socket on the WiFi shield
+  client.stop();
+
+  // if there's a successful connection
+  if (client.connect(server, serverPort)) {
+    delay(500);
+    Serial.println("Connecting...");
+    
+    // send the HTTP PUT request
+    client.println("GET /values HTTP/1.1");
+    client.println("Host: 192.168.0.25:8888");
+    client.println("Connection: keep-alive");
+    client.println();
+
+    // note the time that the connection was made
+    lastConnectionTime = millis();
+  }
+  else {
+    // if you couldn't make a connection
+    Serial.println("Connection failed");
+  }
+}
+
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+String setBaud(String baudrate){
+  String tmpData = "";  //ensure it is empty
+  ESPSerial.println("AT+UART_DEF=" + baudrate + ",8,1,0,0");
+  delay(500);  //good value for wait = 500
+  while (ESPSerial.available() >0 )  {
+    char c = ESPSerial.read();
+    tmpData += c;  // this is better for adding character
+   }
+   return tmpData;
 }
