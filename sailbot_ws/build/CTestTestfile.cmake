f5ec385fe9db7bd81cb11ec321c309f256f4a423
lczebrowski <lczebrowski@wpi.edu>
2020-10-31 16:19:35 -0700

    Added a workspace in which to work on ROS

---
+++ sailbot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nerniply/sailbot20-21/sailbot_ws/src
+# Build directory: /home/nerniply/sailbot20-21/sailbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
