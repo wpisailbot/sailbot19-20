70dc8b3338703a782f003b8d8ece918d1f7486f8
neusman1 <nick.eusman@gmail.com>
2020-11-03 11:45:39 -0500

    remove old unused code, prep for new ros2 workspace

--- sailbot_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nerniply/sailbot20-21/sailbot_ws/src
-# Build directory: /home/nerniply/sailbot20-21/sailbot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basic_pkg")
--- sailbot_ws/build/basic_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nerniply/sailbot20-21/sailbot_ws/src/basic_pkg
-# Build directory: /home/nerniply/sailbot20-21/sailbot_ws/build/basic_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
