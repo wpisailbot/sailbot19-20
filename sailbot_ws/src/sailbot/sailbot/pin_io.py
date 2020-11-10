f5e0440ff2da56175be9fd7aede7b0776f369666
neusman1 <nick.eusman@gmail.com>
2020-11-10 14:59:52 -0500

    this should be a finished skeleton. three methods are left unfinished as they will need to be implemented with actual I/O

--- sailbot_ws/src/sailbot/sailbot/pin_io.py
+++ sailbot_ws/src/sailbot/sailbot/pin_io.py
@@ -9,19 +9,39 @@ class PinIO(Node):
 
     def __init__(self):
         super().__init__('pin_io')
-        #create publisher to serial_rc topic
-        self.publisher_ = self.create_publisher(String, 'serial_rc', 10)
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        #create publisher to serial_rc topic
+        self.serial_rc_publisher_ = self.create_publisher(String, 'serial_rc', 10)
+        self.timer = self.create_timer(timer_period, self.serial_rc_timer_callback)
+        #create subcriber to pwm_control topic
+        self.subscription = self.create_subscription(
+            String,
+            'pwm_control',
+            self.pwm_control_listener_callback,
+            10)
+        self.subscription
+        #create publisher to airmar_data topic
+        self.airmar_data_publisher_ = self.create_publisher(String, 'airmar_data', 10)
+        self.timer = self.create_timer(timer_period, self.airmar_data_timer_callback)
 
-    def timer_callback(self):
+    def serial_rc_timer_callback(self):
         msg = String()
-        msg.data = 'Fake pin output: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        msg.data = check_serial_rc_pins()
+        self.serial_rc_publisher_.publish(msg)
+        self.get_logger().info('Serial RC publishing: "%s"' % msg.data)
 
+    def pwm_control_listener_callback(self, msg):
+        self.get_logger().info('PWM is outputting: "%s"' % msg.data)
+        #TODO: format msg?
+        output_pwm_pins(msg)
+
+    def airmar_data_timer_callback(self):
+        msg = String()
+        msg.data = check_airmar_data()
+        self.airmar_data_publisher_.publish(msg)
+        self.get_logger().info('Airmar data publishing: "%s"' % msg.data)
+        
+        
 
 def main(args=None):
     rclpy.init(args=args)
@@ -35,6 +55,15 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
+
+def check_serial_rc_pins(): #collects and returns json of serial rc pin values
+    return "not yet implemented"
+
+def output_pwm_pins(valArray): #takes json of pwm values and sets pins appropriately
+    print("not yet implemented")
+
+def check_airmar_data(): #collects and returns json of airmar data
+    return "not yet implemented"
 
 
 if __name__ == '__main__':
