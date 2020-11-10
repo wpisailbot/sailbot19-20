2bf04879768161044ea0d5d07a274e43a041a075
neusman1 <nick.eusman@gmail.com>
2020-11-10 15:38:52 -0500

    finished skeletons, not implemented

--- sailbot_ws/src/sailbot/sailbot/control_system.py
+++ sailbot_ws/src/sailbot/sailbot/control_system.py
@@ -8,29 +8,83 @@ class ControlSystem(Node):
 
     def __init__(self):
         super().__init__('control_system')
-        self.subscription = self.create_subscription(
+        #create subcription to serial_rc topic
+        self.serial_rc_subscription = self.create_subscription(
             String,
             'serial_rc',
-            self.listener_callback,
+            self.serial_rc_listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.serial_rc_subscription
+        
+        #create subscription to airmar_data
+        self.airmar_data_subscription = self.create_subscription(
+            String,
+            'airmar_data',
+            self.airmar_data_listener_callback,
+            10)
+        self.airmar_data_subscription
+        
+        #create subscription to teensy_status
+        self.teensy_status_subscription = self.create_subscription(
+            String,
+            'teensy_status',
+            self.teensy_status_listener_callback,
+            10)
+        self.teensy_status_subscription
+        
+        #create publisher to pwm_control
+        self.pwm_control_publisher_ = self.create_publisher(String, 'pwm_control', 10)
 
-    def listener_callback(self, msg):
+        #create publisher to teensy_control
+        self.teensy_control_publisher_ = self.create_publisher(String, 'teensy_control', 10)
+
+        #create instance vars for subscribed topics to update
+        self.serial_rc = ""
+        self.airmar_data = ""
+        self.teensy_status = ""
+        
+
+    def serial_rc_listener_callback(self, msg):
         self.get_logger().info('Received msg: "%s"' % msg.data)
-
+        self.serial_rc = msg
+        
+    def airmar_data_listener_callback(self, msg):
+        self.get_logger().info('Received msg: "%s"' % msg.data)
+        self.airmar_data = msg
+        
+    def teensy_status_listener_callback(self, msg):
+        self.get_logger().info('Received msg: "%s"' % msg.data)
+        self.teensy_status = msg
+        
 
 def main(args=None):
     rclpy.init(args=args)
 
     control_system = ControlSystem()
 
-    rclpy.spin(control_system)
+    while( rclpy.ok() ):
+        rclpy.spin_once(control_system)
+        # now we have new vals from subscribers in:
+        # control_system.serial_rc
+        # control_system.airmar_data
+        # control_system.teensy_status
+
+        # need to publish new values to both control topics based on new values
+        # control_system.pwm_control_publisher_.publish()
+        # control_system.teensy_control_publisher_.publish()
+
+        #TODO ^^implement
+        
+        
+    
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    control_system.destroy_node()
     rclpy.shutdown()
+
+
 
 
 if __name__ == '__main__':
--- sailbot_ws/src/sailbot/sailbot/pin_io.py
+++ sailbot_ws/src/sailbot/sailbot/pin_io.py
@@ -53,7 +53,7 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    pin_io.destroy_node()
     rclpy.shutdown()
 
 def check_serial_rc_pins(): #collects and returns json of serial rc pin values
