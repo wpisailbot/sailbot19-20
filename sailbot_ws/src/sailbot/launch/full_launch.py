1548b62e792c91f1860c7ea0a65558e7427c6c2e
lczebrowski <lczebrowski@wpi.edu>
2020-11-19 09:24:06 -0800

    Update full_launch.py

--- sailbot_ws/src/sailbot/launch/full_launch.py
+++ sailbot_ws/src/sailbot/launch/full_launch.py
@@ -1,19 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# Description:
+# A launch file that simply launches all nodes
+
 def generate_launch_description():
 	return LaunchDescription([
 		Node(
 			package='sailbot',
 			node_executable='pin_io',
-			name='pin_io',
-			output='screen'
+			name='pin_io'
 		),
 		Node(
 			package='sailbot',
 			node_executable='control_system',
-			name='ctrl_sys',
-			output='screen'
+			name='ctrl_sys'
 		),
 		Node(
 			package='sailbot',
