800205e1d13d9775285a322d473aa6634fb11f8d
lczebrowski <lczebrowski@wpi.edu>
2020-12-14 17:18:34 -0800

    debug interface node can send data to dashboard. Needs minor alterations depending on how the data is structured

--- sailbot_ws/src/sailbot/sailbot/debug_interface.py
+++ sailbot_ws/src/sailbot/sailbot/debug_interface.py
@@ -6,35 +6,31 @@ import requests
 
 from std_msgs.msg import String
 
-# Website URL #
-url = '192.168.17.18:3000'
+# Website URL and header #
+url = 'http://localhost:3000/boat'
+Headers = {'Content-type': 'application/json'}
 # Dummy Data #
 pydata1 = {
-    "apparentWind": {"speed": 55, "direction": 90},
-    "theoreticalWind": {"speed": 0, "direction": 270},
-    "compass": {"x": 0, "y": 180, "z": 0},
-    "airtemp": 0,
-    "windchill": 35,
-    "pressure": 950,
-    "groundspeed": 25,
-    "gps": {"latitude": 0.0, "longitude": 4.4, "altitude": 0.0},
-    "pitchroll": {"pitch": -20, "roll": 90},
-    "gyro": {"phi": 4.4, "theta": 0.0, "psi": 4.4}
+    'apparentWind': {'speed': 55, 'direction': 90},
+    'theoreticalWind': {'speed': 0, 'direction': 270},
+    'compass': {'x': 0, 'y': 180, 'z': 0},
+    'groundspeed': 25,
+    'gps': {'latitude': 0.0, 'longitude': 4.4, 'altitude': 0.0},
+    'pitchroll': {'pitch': -20, 'roll': 90},
+    'gyro': {'phi': 4.4, 'theta': 0.0, 'psi': 4.4}
 }
 data1 = json.dumps(pydata1)
 pydata2 = {
     "apparentWind": {"speed": 0, "direction": 270},
     "theoreticalWind": {"speed": 55, "direction": 90},
     "compass": {"x": 180, "y": 0, "z": 0},
-    "airtemp": 35,
-    "windchill": 0,
-    "pressure": 1050,
     "groundspeed": 0,
     "gps": {"latitude": 4.4, "longitude": 0.0, "altitude": 4.4},
     "pitchroll": {"pitch": 20, "roll": -90},
     "gyro": {"phi": 0.0, "theta": 4.4, "psi": 0.0}
 }
 data2 = json.dumps(pydata2)
+
 
 class DebugInterface(Node):
 
@@ -78,7 +74,10 @@ class DebugInterface(Node):
     def airmar_data_listener_callback(self, msg):
         self.get_logger().info('Airmar data: "%s"' % msg.data)
         self.airmar_data = msg
-        #requests.post(url, json = msg.data)
+        # if data is in json format:
+        #r = requests.post(url, data=(DATA_HERE), headers=Headers)
+        # if data is in python dict format:
+	#r = requests.post(url, json=(DATA_HERE))
         
     def teensy_status_listener_callback(self, msg):
         self.get_logger().info('Teensy msg: "%s"' % msg.data)
