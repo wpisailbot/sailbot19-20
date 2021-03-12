311913f3d1039ae6ac88b2f5df7fd047be52c360
neusman1 <nick.eusman@gmail.com>
2021-03-12 01:17:40 -0500

    fix

--- sailbot_ws/src/sailbot/sailbot/debug_interface.py
+++ sailbot_ws/src/sailbot/sailbot/debug_interface.py
@@ -8,7 +8,7 @@ from std_msgs.msg import String
 
 # Website URL and header #
 ip = '192.168.17.20'
-url = ip + ':3000/boat'
+url = 'http://' + ip + ':3000/boat'
 Headers = {'Content-type': 'application/json'}
 
 
