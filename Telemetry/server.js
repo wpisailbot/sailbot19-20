81c0c8da7814548ef24c40c5aa88f787fd209b6a
athammana <aj@virpietech.com>
2021-03-17 18:31:40 -0400

    Updating stuff so it works with new JSON keys

--- Telemetry/server.js
+++ Telemetry/server.js
@@ -2,20 +2,42 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
+const url = "mongodb+srv://sailbothot:admin@cluster0.ay01x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const client = new MongoClient(url);
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-let db = [];
+let countData = true;
+let dataTimeDiff = 0;
+
+let collection = undefined;
+const dbName = "Sailbot";
+
 
 app.post('/boat', (req, res) => {
-	console.log(req.body);
-	io.to('clients').emit('updateDash', req.body);
+
+	if (countData) {
+		let data = req.body;
+		countData = false;
+
+		if (data.hasOwnProperty('latitude'))
+			io.to('clients').emit('updateAirmarDash', req.body);
+		else if (data.hasOwnProperty('angle'))
+			io.to('clients').emit('updateTrimDash', req.body);
+		else if (data.hasOwnProperty('rudder'))
+			io.to('clients').emit('updateSerialControls', req.body);
+
+		addToDB(data);
+		setTimeout(() => countData = true, dataTimeDiff);
+	}
 	res.send(200);
 });
 
@@ -31,10 +53,19 @@ io.on('connection', (socket) => {
 	});
 });
 
-// Function that will add to a database in the future
-const addToDB = (data) => {
-	db.push(data); // implement real online and offline database eventually
+const startDB = async () => {
+	await client.connect();
+	const db = client.db(dbName);
+	collection = db.collection("TestData");     
+	console.log("Connected correctly to collection");
 }
 
+// Function that will add to a database in the future
+const addToDB = async (data) => {
+	db.push(data); // implement real online and offline database eventually
+	await col.insertOne(data);
+}
+
+startDB();
 // Server listen function
 server.listen(port, () => console.log('Listening on port: ' + port));
